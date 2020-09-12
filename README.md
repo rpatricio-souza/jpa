8227cc0d589e32494e0df4fb662714cdb6a1de40
rpatricio-souza <rpatricio.souza@gmail.com>
2020-09-12 02:52:41 +0000

    updates

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # jpa
 Laboratório de Desenvolvimento de Banco de Dados IV - Atividade 02 - 4º semestre - 2020 Fatec SJC
+
+Exemplo do uso de JPA
+
+# Compilação
+Para compilar use: mvn compile
+
+# Execução
+Para executar use: mvn exec:java -Dexec.mainClass="br.gov.sp.fatec.projetomaven.App"
--- src/main/java/br/gov/sp/fatec/projetomaven/App.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/App.java
@@ -1,5 +1,11 @@
 package br.gov.sp.fatec.projetomaven;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.gov.sp.fatec.projetomaven.entity.Aluno;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        EntityManagerFactory factory = Persistence
+        .createEntityManagerFactory("avaliacao");
+        EntityManager manager = factory.createEntityManager();
+        
+        Aluno aluno = new Aluno();
+        aluno.setNomeUsuario("aluno");
+        aluno.setSenha("senha");
+        aluno.setRa(1234567891011L);
+
+        try {
+            manager.getTransaction().begin();
+            manager.persist(aluno);
+            manager.getTransaction().commit();
+        } catch(Exception e) {
+            manager.getTransaction().rollback();
+        }
+        System.out.println(aluno.getId());
+        manager.close();
     }
 }
